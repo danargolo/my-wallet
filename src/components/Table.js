97ae60083bc3f04cae0b2b6fd225c978b265797f
Danilo Argolo <dan_argolo@hotmail.com>
2022-12-14 13:29:31 -0300

    Requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+    console.log(expenses);
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          { expenses.map((exp) => (
+            <tr key={ exp.id }>
+              <td>{ exp.description }</td>
+              <td>{ exp.tag }</td>
+              <td>{ exp.method }</td>
+              <td>{ (+exp.value).toFixed(2) }</td>
+              <td>{ exp.exchangeRates[exp.currency].name }</td>
+              <td>{ (+exp.exchangeRates[exp.currency].ask).toFixed(2) }</td>
+              <td>
+                { ((+exp.exchangeRates[exp.currency].ask) * (exp.value)).toFixed(2) }
+              </td>
+              <td>Real</td>
+            </tr>
+          )) }
+        </tbody>
       </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
